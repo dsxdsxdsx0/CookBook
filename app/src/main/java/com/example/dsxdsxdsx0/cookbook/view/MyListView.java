8787a17fe862c9dc9b33921983673d3fb2aa8ea0
dsx <1553267648@qq.com>
2016-09-01 10:13:12 +0800

    自定义ListView，用来展示做菜的步骤

---
+++ app/src/main/java/com/example/dsxdsxdsx0/cookbook/view/MyListView.java
@@ -0,0 +1,35 @@
+package com.example.dsxdsxdsx0.cookbook.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by dsxdsxdsx0 on 2016/8/31.
+ */
+public class MyListView extends ListView {
+
+    public MyListView(Context context) {
+        super(context);
+    }
+
+    public MyListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //size给出父类最大的高度，
+        //  MeasureSpec.AT_MOST是最大尺寸，当控件的layout_width或layout_height指定为WRAP_CONTENT时，
+        //控件大小一般随着控件的子空间或内容进行变化，此时控件尺寸只要不超过父控件允许的最大尺寸即可。
+        //因此，此时的mode是AT_MOST，size给出了父控件允许的最大尺寸。
+        int mExpandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, mExpandSpec);
+
+    }
+}
